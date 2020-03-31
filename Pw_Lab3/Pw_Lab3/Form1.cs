2b7272d20ae18a1c1b9458c4700dd0ac0cc7c229
MichalChojnacki450 <58258243+MichalChojnacki450@users.noreply.github.com>
2020-03-31 11:21:31 +0200

    Add files via upload

---
+++ Pw_Lab3/Pw_Lab3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pw_Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if((textBox1.Text !="") && (textBox2.Text !="") && (textBox4.Text !=""))
+            {
+                label1.ForeColor = Color.Black;
+                float waga = Convert.ToInt32(textBox1.Text);
+                float wzrost = Convert.ToInt32(textBox2.Text);
+                float wiek = Convert.ToInt32(textBox4.Text);
+                if (textBox3.Text == "M")
+                {
+                    Single bmr = ((float)9.99 * waga) + (wzrost * (float)6.25) - (wiek * (float)4.92) + 5;
+                    label1.Text = bmr.ToString();
+                }
+                if(textBox3.Text == "K")
+                {
+                    Single bmr = ((float)9.99 * waga) + (wzrost * (float)6.25) - (wiek * (float)4.92) - 161;
+                    label1.Text = bmr.ToString();
+                }
+                else
+                { 
+                    label1.ForeColor = Color.Red;
+                    label1.Text = "Złe lub brak danych";
+                }
+            }
+            else
+            { 
+                label1.ForeColor = Color.Red;
+                label1.Text = "Brak danych";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Pw_Lab3/Pw_Lab3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Pw_Lab3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pw_Lab3")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("f2c641fb-3904-4a8e-9056-dc31ff32322f")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
