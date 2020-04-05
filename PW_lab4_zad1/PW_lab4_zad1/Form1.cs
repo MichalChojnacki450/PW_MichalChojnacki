1ba89e06345086252331b0e8e77f4dafaad0458b
MichalChojnacki450 <58258243+MichalChojnacki450@users.noreply.github.com>
2020-04-05 17:34:32 +0200

    Add files via upload

---
+++ PW_lab4_zad1/PW_lab4_zad1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PW_lab4_zad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2(textBox1.Text,textBox2.Text,textBox3.Text,textBox4.Text,textBox5.Text);
+            frm.Show();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PW_lab4_zad1/PW_lab4_zad1/Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PW_lab4_zad1
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string strTextBox, string strTextBox1, string strTextBox2 ,string strTextBox3 ,string strTextBox4)
+        {
+            InitializeComponent();
+            label1.Text = strTextBox;
+            label2.Text = strTextBox1;
+            label3.Text = strTextBox2;
+            label4.Text = strTextBox3;
+            label5.Text = strTextBox4;
+        }
+    }
+}
